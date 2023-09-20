89b5b3d56df92560e3fd0d5f3ab708d61cafdb0b
Nelofar Zabi <nelofarzabi2000@gmail.com>
2023-09-20 10:00:14 +0430

    Add new data to animal table

--- data.sql
+++ data.sql
@@ -6,3 +6,14 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
         ('Gabumon', '2018-11-15', 2, TRUE, 8.00),
         ('Pikachu', '2021-01-07', 1, FALSE, 15.04),
         ('Devimon', '2017-05-12', 5, TRUE, 11.00)
+
+
+
+INSERT INTO animals  (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES
+        ('Charmander', '2020-02-08', 0, FALSE, -11.00),
+        ('Plantmon', '2021-11-15', 2, TRUE, -5.70),
+        ('Squirtle', '1993-04-02', 3, FALSE, -12.13),
+        ('Angemon', '2005-06-12', 1, TRUE, -45.00),
+        ('Boarmon', '2005-06-07', 7, TRUE, 20.40),
+        ('Blossom', '1998-10-13', 3, TRUE, 17.00),
+        ('Ditto', '2022-05-14', 4, TRUE, 22.00);
