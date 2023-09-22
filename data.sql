43bee9f17edaa322cbcc403c13ec40d33add3e93
Nelofar Zabi <nelofarzabi2000@gmail.com>
2023-09-22 07:49:45 +0430

    Insert data to owner table

--- data.sql
+++ data.sql
@@ -17,3 +17,12 @@ INSERT INTO animals  (name, date_of_birth, escape_attempts, neutered, weight_kg)
         ('Boarmon', '2005-06-07', 7, TRUE, 20.40),
         ('Blossom', '1998-10-13', 3, TRUE, 17.00),
         ('Ditto', '2022-05-14', 4, TRUE, 22.00);
+
+-- Insert the following data into the owners table:
+INSERT INTO owners (full_name, age) VALUES
+	('Sam Smith', 34),
+	('Jennifer Orwell', 19),
+	('Bob', 45),
+	('Melody Pond', 77),
+	('Dean Winchester', 14),
+	('Jodie Whittaker', 38);
