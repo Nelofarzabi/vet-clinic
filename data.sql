7fd32adae4afbb7c425009f19e7eae112babc850
Nelofar Zabi <nelofarzabi2000@gmail.com>
2023-09-22 07:53:27 +0430

    Remove spaces

--- data.sql
+++ data.sql
@@ -1,4 +1,3 @@
-
 /* Populate database with sample data. */
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES 
@@ -6,8 +5,6 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
         ('Gabumon', '2018-11-15', 2, TRUE, 8.00),
         ('Pikachu', '2021-01-07', 1, FALSE, 15.04),
         ('Devimon', '2017-05-12', 5, TRUE, 11.00)
-
-
 
 INSERT INTO animals  (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES
         ('Charmander', '2020-02-08', 0, FALSE, -11.00),
