7154bad6cae97a82351404a8a59e2c202973bcc0
Nelofar Zabi <nelofarzabi2000@gmail.com>
2023-09-22 07:52:43 +0430

    Add data to animal

--- data.sql
+++ data.sql
@@ -61,3 +61,8 @@ WHERE name IN ('Gabumon', 'Pikachu');
 UPDATE animals
 SET owner_id = (SELECT id FROM owners WHERE full_name = 'Bob')
 WHERE name IN ('Devimon', 'Plantmon');
+
+-- Melody Pond 
+UPDATE animals
+SET owner_id = (SELECT id FROM owners WHERE full_name = 'Melody Pond')
+WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
