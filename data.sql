056de03c9949e63effa24c093ebdeadc1e143f2b
Nelofar Zabi <nelofarzabi2000@gmail.com>
2023-09-22 09:27:34 +0430

    Insert data for vets

--- data.sql
+++ data.sql
@@ -68,3 +68,10 @@ WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
 UPDATE animals
 SET owner_id = (SELECT id FROM owners WHERE full_name = 'Dean Winchester')
 WHERE name IN ('Angemon', 'Boarmon');
+
+-- Insert the following data for vets
+INSERT INTO vets (name, age, date_of_graduation) VALUES
+('William Tatcher', 45, '2000-04-23'),
+('Maisy Smith', 26, '2019-01-17'),
+('Stephanie Mendez', 64, '1981-05-04'),
+('Jack Harkness', 38, '2008-06-08');
